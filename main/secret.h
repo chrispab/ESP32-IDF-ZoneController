a2bbcd738ca2ea0b291f71b0d5a56605b76db351
chrispab <cbattisson@gmail.com>
2018-12-07 00:57:04 +0000

    port 1

---
+++ main/secret.h
@@ -0,0 +1,6 @@
+#define MYSSID "notwork"
+#define MYWIFIPASSWORD "a new router can solve many problems"
+
+#define GUSERNAME "cbattisson@gmail.com"
+#define GAPPPASSWORD "fbmfbmqluzaakvso"
+#define GMAILTO "cbattisson@gmail.com"
